dafb3a0e45a94c589eac5b11215dbe45422e9933
darth-roman <chouchane.louai20@gmail.com>
2021-10-22 00:01:19 +0200

    first commit

---
+++ sessions/hello.c
@@ -0,0 +1,15 @@
+// stdio = Standard Input Output
+#include<stdio.h>
+
+
+// Procedural Paradigm, OOP, Logical ...
+// MANY MANY OF LANGUAGES
+// R. Python MachineL and Data Science, C, C++
+// C is God's programming language
+
+// Return Type
+
+int main(){
+    printf("Hello World\n");
+    return -1;
+}
---
+++ sessions/isPair.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+void main(){
+    // "Even" = Pair, "Odd" = impair
+    // n % 2 = 0 
+    // Ba9i al 9issma = 0 ==> nombre impair
+    // The user will give us the integer (n)
+    int n, rest;
+    printf("Enter your number\n");
+    scanf("%d", &n);
+    rest = n % 2;
+    if(rest == 0){ // Boolean = True or False, 1 or 0
+        printf("%d is Even\n", n);
+    }else{
+        printf("%d is Odd\n", n);
+    }
+}
