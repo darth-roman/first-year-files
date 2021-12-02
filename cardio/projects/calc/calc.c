0e996497f8229283a49bccf1dc42cdd6f7e168a4
darth-roman <chouchane.louai20@gmail.com>
2021-12-02 18:21:35 +0100

    comments to explain the code

--- cardio/projects/calc/calc.c
+++ cardio/projects/calc/calc.c
@@ -1,24 +1,38 @@
 #include <stdio.h>
-
+// a simple and basic version of printf()
+//  that adds the \n char at the end of the string
 void printlnf(char *string)
 {
     printf("%s\n", string);
 }
 
+/*
+    * function names starts with float handle 
+    the arithmetics of float args with a float return type
+
+    * function names starts with int handle 
+    the arithmetics of integer args with a int return type
+*/
+
+// returns the addition of two floats a and b
 float float_add(float a, float b)
 {
     return a + b;
 }
 
+// returns the multiplication of two floats a and b
 float float_multiply(float a, float b)
 {
     return a * b;
 }
 
+// returns the substraction of two floats a and b
 float float_substract(float a, float b)
 {
     return a - b;
 }
+
+// returns the division of two floats a and b
 float float_divide(float a, float b)
 {
     if (b == 0)
@@ -30,20 +44,25 @@ float float_divide(float a, float b)
     return a / b;
 }
 
+// returns the addition of two floats a and b
 int int_add(int a, int b)
 {
     return a + b;
 }
 
+// returns the multiplication of two ints a and b
 int int_multiply(int a, int b)
 {
     return a * b;
 }
 
+// returns the substraction of two int(s) a and b
 int int_substract(int a, int b)
 {
     return a - b;
 }
+
+// returns the division of two int(s) a and b
 int int_divide(int a, int b)
 {
     if (b == 0)
